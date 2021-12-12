8afca156c91e5c860fb0ce2f3a6797d57fb488c5
rafael fernandes <rafamex.mex@gmail.com>
2021-12-12 01:28:06 -0300

    Game v1 complet

--- js/index.js
+++ js/index.js
@@ -1,8 +1,14 @@
 const canvas = document.querySelector('canvas')
+const c = canvas.getContext('2d');
+
 canvas.width = innerWidth;
 canvas.height = innerHeight - 10;
 
-const c = canvas.getContext('2d');
+const scoreEl = document.querySelector('#scoreEl')
+const startGameBtn = document.querySelector('#startGameBtn')
+const modalEl = document.querySelector('#modalEl')
+const bigScoreEl = document.querySelector('#bigScoreEl')
+
 
 class Player {
     constructor(x, y, radius, color) {
@@ -60,7 +66,6 @@ class Enemy {
         c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
         c.fillStyle = this.color;
         c.fill();
-        c.closePath();
     }
 
     update() {
@@ -70,7 +75,7 @@ class Enemy {
     }
 }
 
-
+const friction = 0.97;
 class Particle {
     constructor(x, y, radius, color, velocity) {
         this.x = x;
@@ -78,30 +83,46 @@ class Particle {
         this.radius = radius;
         this.color = color;
         this.velocity = velocity;
+        this.alpha = 1;
     }
 
     draw() {
+        c.save()
+        c.globalAlpha = this.alpha;
         c.beginPath();
         c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
         c.fillStyle = this.color;
         c.fill();
-        c.closePath();
+        c.restore()
     }
 
     update() {
         this.draw();
+        this.velocity.x *= friction;
+        this.velocity.y *= friction;
         this.x += this.velocity.x;
         this.y += this.velocity.y;
+        this.alpha -= 0.01;
     }
 }
 
 const x = canvas.width / 2;
 const y = canvas.height / 2;
 
-const player = new Player(x,y, 10, 'white');
-const projectiles = [];
-const enemies = [];
-const particles = [];
+let player = new Player(x,y, 10, 'white');
+let projectiles = [];
+let enemies = [];
+let particles = [];
+
+function init() {
+    player = new Player(x, y, 10, 'white');
+    projectiles = [];
+    enemies = [];
+    particles = [];
+    score = 0;
+    scoreEl.innerHTML = score;
+    bigScoreEl.innerHTML = score;
+}
 
 function spawnEnemies() {
 
@@ -129,13 +150,18 @@ function spawnEnemies() {
 }
 
 let animationId;
+let score = 0;
 function animate() {
     animationId = requestAnimationFrame(animate);
     c.fillStyle = 'rgba(0,0,0,0.1)'
     c.fillRect(0,0, canvas.width, canvas.height);
     player.draw();
-    particles.forEach(particle => {
-        particles.update()
+    particles.forEach((particle, particleIndex) => {
+        if (particle.alpha <= 0) {
+            particles.splice(particleIndex, 1)
+        }else{
+            particle.update()
+        }
     });
 
     projectiles.forEach((projectile, projectileIndex) => {
@@ -157,8 +183,18 @@ function animate() {
 
         const distEnemyPlayer = Math.hypot(player.x - enemy.x, player.y - enemy.y);
 
-        if (distEnemyPlayer - enemy.radius - player.radius < 1) { //colisão enemy player
-            cancelAnimationFrame(animationId)
+        //colisão enemy player
+        if (distEnemyPlayer - enemy.radius - player.radius < 1) {
+            //cria o efeito de explosão do player
+            for (let i = 0; i < player.radius * 2; i++) {
+                particles.push(new Particle(player.x, player.y, Math.random() * 2, player.color, { x: (Math.random() - 0.5) * (Math.random() * 6), y: (Math.random() - 0.5) * (Math.random() * 6) }))
+            }
+            enemy.velocity ={x:0, y:0}
+            setTimeout(() => {
+                cancelAnimationFrame(animationId);
+                modalEl.style.display = 'flex';
+                bigScoreEl.innerHTML = score;
+            }, 1000);
         }
 
         projectiles.forEach((projectile, projectileIndex) => {
@@ -167,11 +203,16 @@ function animate() {
             //colisão projetil enemy
             if (distProjectileEnemy - enemy.radius - projectile.radius < 1) {
 
-                for (let i = 0; i < 8; i++) {
-                    projectiles.push(new Particle(projectile.x, projectile.y, 3, enemy.color, { x: Math.random() - 0.5, y: Math.random() - 0.5}))
+                //cria o efeito de explosão
+                for (let i = 0; i < enemy.radius*2; i++) {
+                    particles.push(new Particle(projectile.x, projectile.y, Math.random() * 2, enemy.color, { x: (Math.random() - 0.5) * (Math.random() * 6), y: (Math.random() - 0.5) * (Math.random() * 6)}))
                 }
 
-                if (enemy.radius -10 > 5) {
+                if (enemy.radius - 10 > 5) {
+                    //aumenta o score
+                    score += 100;
+                    scoreEl.innerHTML = score;
+
                     gsap.to(enemy,{
                         radius: enemy.radius - 10
                     })
@@ -179,6 +220,10 @@ function animate() {
                         projectiles.splice(projectileIndex, 1)//remove o projetil
                     }, 0);
                 }else{
+                    //aumenta o score em +250 por remover da tela (kill)
+                    score += 250;
+                    scoreEl.innerHTML = score;
+
                     setTimeout(() => {//tira o efeito de "flash" da colisão
                         enemies.splice(enemyIndex,1)//remove o inimigo
                         projectiles.splice(projectileIndex,1)//remove o projetil
@@ -190,15 +235,48 @@ function animate() {
 
 }
 
+let full = false;
 
-addEventListener('click', (event) => {
+addEventListener('keypress', (event) =>{
+    let key = event.which || event.keyCode;
+    if (key === 101) {
+        full = !full;
+        console.log(full)
+    }
+
+    if (full) {
+        addEventListener('mousedown', addFireToWindown)
+        addEventListener('mouseup', removeFireToWindown)
+    }else{
+        window.removeEventListener('mousedown', addFireToWindown)
+        window.removeEventListener('mouseup', removeFireToWindown)
+        window.removeEventListener('mousemove', fire)
+    }
+})
+
+function addFireToWindown() {
+    window.addEventListener('mousemove', fire)
+}
+
+function removeFireToWindown() {
+    window.addEventListener('mousemove', fire)
+}
+
+function fire(event){
     const angle = Math.atan2(event.clientY - canvas.height / 2, event.clientX - canvas.width / 2);
     const velocity = {
         x: Math.cos(angle) * 5,
         y: Math.sin(angle) * 5
     }
     projectiles.push(new Projectile(player.x, player.y, 5, 'white', velocity));
-})
+}
 
-animate();
-spawnEnemies();
+
+addEventListener('click', fire)
+
+startGameBtn.addEventListener('click', () =>{
+    init()
+    animate();
+    spawnEnemies();
+    modalEl.style.display = 'none';
+})
